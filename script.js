7f79c08fe21e6a4de7f37b41165f0a358d65aaa9
sc-forlife <salemsc7@gmail.com>
2026-04-28 18:50:57 +0200

    Implementing the highlight matched text feature

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const regexPattern = document.getElementById("pattern");
 const stringToTest = document.getElementById("test-string");
 const testButton = document.getElementById("test-btn");
 const testResult = document.getElementById("result");
+const globalFlags = document.getElementById("g");
 
 //return flags
 function getFlags() {
   const caseInsensitiveFlag = document.getElementById("i");
-  const globalFlags = document.getElementById("g");
 
   const iChecked = caseInsensitiveFlag.checked ? "i" : "";
   const gChecked = globalFlags.checked ? "g" : "";
@@ -20,10 +20,33 @@ function getRegex(pattern) {
   return regex;
 }
 
-testButton.addEventListener("click", () => {
-  const regex = getRegex(regexPattern.value);
-  const string = stringToTest.value;
+function getRegexMatch(regex, string) {
   const matched = string.match(regex);
 
-  console.log(Array.from(matched));
+  return globalFlags.checked ? matched.join(",") : matched[0];
+}
+
+function highlightText(text) {
+  let highlight = "";
+  let newText = "";
+  const textArr = text.split("");
+
+  for (letter of textArr) {
+    if (getRegex(regexPattern.value).test(letter)) {
+      highlight += letter;
+      continue;
+    } else {
+      newText += `<span class="\highlight\">${highlight}</span>${letter}`;
+      highlight = "";
+      continue;
+    }
+  }
+  console.log(newText);
+  return newText;
+}
+
+testButton.addEventListener("click", () => {
+  const pattern = getRegex(regexPattern.value);
+  testResult.innerText = getRegexMatch(pattern, stringToTest.innerText);
+  stringToTest.innerHTML = highlightText(stringToTest.innerText);
 });
