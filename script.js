ec4017affd112792f4aeaef0e628e5ac0d956457
sc-forlife <salemsc7@gmail.com>
2026-04-30 06:57:46 +0200

    Completed challenge including the replace highlight feature

--- script.js
+++ script.js
@@ -16,48 +16,34 @@ function getFlags() {
   return flags;
 }
 
-function getRegexMatch(string) {
+function getExp() {
   const regex = new RegExp(`${regexPattern.value}`, `${getFlags()}`);
+  return regex;
+}
+
+function getRegexMatch(string) {
+  const regex = getExp();
   const matched = string.match(regex);
 
   if (matched === null) {
     return "no match";
   }
   console.log(matched);
-  return globalFlag.checked ? matched : [...matched[0]];
+  return globalFlag.checked ? matched.join(", ") : matched[0];
 }
 
 function highlightText(userString) {
-  const matchedString = getRegexMatch(stringToTest.innerText);
+  const regex = getExp();
 
-  if (matchedString === null) return userString;
-
-  console.log(matchedString);
-
-  let highlight;
-  let newWord = userString;
-
-  for (const matched of matchedString) {
-    highlight = matched.replace(
-      matched,
-      `<span class=\"highlight\">${matched}</span>`,
-    );
-    if (newWord.includes(highlight)) {
-      continue;
-    } else {
-      if (globalFlag.checked) {
-        newWord = newWord.replaceAll(matched, highlight);
-      } else {
-        newWord = newWord.replace(matched, highlight);
-      }
-    }
-  }
+  const newWord = userString.replace(regex, (matched) => {
+    return `<span class=\"highlight\">${matched}</span>`;
+  });
 
   return newWord;
 }
 
 testButton.addEventListener("click", () => {
-  testResult.textContent = getRegexMatch(stringToTest.innerText).join(", ");
+  testResult.textContent = getRegexMatch(stringToTest.innerText);
 
   console.log(highlightText(stringToTest.innerText));
 
